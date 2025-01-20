f74de3d9ed19030ce17580bbb2e4087f237edf62
josephkerkhof <5824820+josephkerkhof@users.noreply.github.com>
2025-01-20 19:59:52 +0000

    Fix styling

--- database/migrations/create_audit_table.php
+++ database/migrations/create_audit_table.php
@@ -2,41 +2,40 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     public function up(): void
-        {
-            Schema::create('audits', function (Blueprint $table) {
-                // Primary key
-                $table->id();
+    {
+        Schema::create('audits', function (Blueprint $table) {
+            // Primary key
+            $table->id();
 
-                // Polymorphic relation fields
-                $table->string('auditable_type');
-                $table->string('auditable_id');
+            // Polymorphic relation fields
+            $table->string('auditable_type');
+            $table->string('auditable_id');
 
-                // Audit metadata
-                $table->string('event'); // 'created', 'updated', 'deleted'
+            // Audit metadata
+            $table->string('event'); // 'created', 'updated', 'deleted'
 
-                // Changed data
-                $table->json('old_values')->nullable();
-                $table->json('new_values')->nullable();
+            // Changed data
+            $table->json('old_values')->nullable();
+            $table->json('new_values')->nullable();
 
-                // Timestamps
-                $table->timestamp('created_at')->useCurrent();
+            // Timestamps
+            $table->timestamp('created_at')->useCurrent();
 
-                // Regular indexes
-                $table->index(['auditable_type', 'auditable_id'], 'audits_auditable_index');
-                $table->index(['user_type', 'user_id'], 'audits_user_index');
-                $table->index('event');
-                $table->index('created_at'); // could be improved by using a partial index like YYYY-MM
-            });
-        }
+            // Regular indexes
+            $table->index(['auditable_type', 'auditable_id'], 'audits_auditable_index');
+            $table->index(['user_type', 'user_id'], 'audits_user_index');
+            $table->index('event');
+            $table->index('created_at'); // could be improved by using a partial index like YYYY-MM
+        });
+    }
 
-        public function down(): void
-        {
-            Schema::dropIfExists('audits');
-        }
+    public function down(): void
+    {
+        Schema::dropIfExists('audits');
+    }
 };

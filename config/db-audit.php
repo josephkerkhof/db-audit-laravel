809977e3ba63b0b5770264d3ea56d0fd20f78d47
Joseph Kerkhof <joseph@kerkhof.dev>
2025-01-20 21:09:17 -0600

    add model resolver

--- config/db-audit.php
+++ config/db-audit.php
@@ -6,4 +6,12 @@ return [
      * The fully qualified class name of the audit model.
      */
     'audit_model' => JosephKerkhof\DbAudit\Models\Audit::class,
+
+    /*
+     * The locations to search for models to audit.
+     */
+    'model_locations' => [
+        'App\\Models',
+        'App',
+    ],
 ];

6ca3e7aa6c30bd6489345e8b306a3d48b2393419
Joseph Kerkhof <joseph@kerkhof.dev>
2025-01-21 20:08:54 -0600

    add test to sqlite grammar

--- src/Database/Schema/Grammars/SQLiteGrammar.php
+++ src/Database/Schema/Grammars/SQLiteGrammar.php
@@ -2,9 +2,15 @@
 
 namespace JosephKerkhof\DbAudit\Database\Schema\Grammars;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Grammars\SQLiteGrammar as Base;
+use Illuminate\Support\Fluent;
 
 class SQLiteGrammar extends Base
 {
-
+    public function compileCreate(Blueprint $blueprint, Fluent $command)
+    {
+        dd('i am trying to create');
+        return parent::compileCreate($blueprint, $command);
+    }
 }
